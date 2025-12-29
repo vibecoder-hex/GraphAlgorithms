44f9afaac82a729770c03d7f028d8b97eedbf42e
coder6497 <92335331+coder6497@users.noreply.github.com>
2025-12-29 12:55:25 +0300

    Rename project file

--- GraphProcessor/Program.cs
+++ GraphProcessor/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 
-namespace ConsoleApp1
+namespace GraphProcessor
 {
     // JSON data series
     public record DistanceData(Dictionary<string, Dictionary<string, int>> Distances); 
